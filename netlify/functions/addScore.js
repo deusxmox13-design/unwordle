b87ce8f3425b898568774884172e17e028a75fd8
deusxmox13-design <deusxmox13@gmail.com>
2025-12-24 20:47:47 -0500

    Update addScore.js

--- netlify/functions/addScore.js
+++ netlify/functions/addScore.js
@@ -9,8 +9,8 @@ export async function handler(event, context) {
   }
 
   const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_KEY
+    process.env.https://grtfmpjuktcabembuzla.supabase.co,
+    process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdydGZtcGp1a3RjYWJlbWJ1emxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2MjIwMzEsImV4cCI6MjA4MjE5ODAzMX0.sk3cQv8XpcVGMrqSVgeGgW-kbu5oiNQBGRxe2z46wLE
   );
 
   const { error } = await supabase
@@ -26,3 +26,4 @@ export async function handler(event, context) {
     body: JSON.stringify({ success: true })
   };
 }
+
