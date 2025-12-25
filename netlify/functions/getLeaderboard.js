991b4f16729ffeb5c722cc37bb83fda1a4befb6a
deusxmox13-design <deusxmox13@gmail.com>
2025-12-24 20:48:17 -0500

    Update getLeaderboard.js

--- netlify/functions/getLeaderboard.js
+++ netlify/functions/getLeaderboard.js
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 export async function handler(event, context) {
   const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_KEY
+    process.env.https://grtfmpjuktcabembuzla.supabase.co,
+    process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdydGZtcGp1a3RjYWJlbWJ1emxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2MjIwMzEsImV4cCI6MjA4MjE5ODAzMX0.sk3cQv8XpcVGMrqSVgeGgW-kbu5oiNQBGRxe2z46wLE
   );
 
   const { data, error } = await supabase
@@ -21,3 +21,4 @@ export async function handler(event, context) {
     body: JSON.stringify(data)
   };
 }
+
